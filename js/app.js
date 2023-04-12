e0da957b2abd417083143fb704b7d631192af141
alex-ngr1 <sashafo01928@gmail.com>
2023-04-13 01:59:29 +0300

    my first commit '_'

---
+++ js/app.js
@@ -0,0 +1,16 @@
+new Swiper('.slider', {
+	// direction: 'vertical',
+	// freeMode: true,
+	loop: true,
+	speed: 2400,
+	parallax: true,
+	spaceBetween: 18,
+	keyboard: {
+	  enabled: true,
+	  onlyInViewport: false,
+	},
+	mousewheel: {
+	  enabled: true,
+	  sensitivity: 2.4,
+	},
+  });
